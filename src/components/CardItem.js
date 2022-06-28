b7a0803efc760cab15f23c062b72872294561997
Claiirou <clairemachet@hotmail.fr>
2022-06-29 00:43:27 +0200

    before merge

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -3,6 +3,8 @@ import React from "react";
 export default function CardItem({ item, id, handleClick }) {
   const itemClass = item.stat ? " active " + item.stat : "";
 
+  console.log("titiCardItem", item);
+
   return (
     <div className={"card" + itemClass} onClick={() => handleClick(id)}>
       <img src={item.img} alt="item.id" />
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,22 +4,22 @@ import CardItem from "./CardItem";
 export default function Cards() {
   const [items, setItems] = useState(
     [
-      { id: 1, img: "/img/belier.jpg", stat: "" },
-      { id: 1, img: "/img/belier.jpg", stat: "" },
-      { id: 2, img: "/img/capricorne.jpg", stat: "" },
-      { id: 2, img: "/img/capricorne.jpg", stat: "" },
-      { id: 3, img: "/img/gemeaux.jpg", stat: "" },
-      { id: 3, img: "/img/gemeaux.jpg", stat: "" },
-      { id: 4, img: "/img/lion.jpg", stat: "" },
-      { id: 4, img: "/img/lion.jpg", stat: "" },
-      { id: 5, img: "/img/sagittaire.jpg", stat: "" },
-      { id: 5, img: "/img/sagittaire.jpg", stat: "" },
-      { id: 6, img: "/img/scorpion.jpg", stat: "" },
-      { id: 6, img: "/img/scorpion.jpg", stat: "" },
-      { id: 7, img: "/img/taureau.jpg", stat: "" },
-      { id: 7, img: "/img/taureau.jpg", stat: "" },
-      { id: 8, img: "/img/verseau.jpg", stat: "" },
-      { id: 8, img: "/img/verseau.jpg", stat: "" },
+      { id: 1, img: "/img/belier.png", stat: "" },
+      { id: 1, img: "/img/belier.png", stat: "" },
+      { id: 2, img: "/img/capricorne.png", stat: "" },
+      { id: 2, img: "/img/capricorne.png", stat: "" },
+      { id: 3, img: "/img/gemeaux.png", stat: "" },
+      { id: 3, img: "/img/gemeaux.png", stat: "" },
+      { id: 4, img: "/img/lion.png", stat: "" },
+      { id: 4, img: "/img/lion.png", stat: "" },
+      { id: 5, img: "/img/sagittaire.png", stat: "" },
+      { id: 5, img: "/img/sagittaire.png", stat: "" },
+      { id: 6, img: "/img/scorpion.png", stat: "" },
+      { id: 6, img: "/img/scorpion.png", stat: "" },
+      { id: 7, img: "/img/taureau.png", stat: "" },
+      { id: 7, img: "/img/taureau.png", stat: "" },
+      { id: 8, img: "/img/verseau.png", stat: "" },
+      { id: 8, img: "/img/verseau.png", stat: "" },
     ].sort(() => Math.random() - 0.5)
   );
 
@@ -53,6 +53,8 @@ export default function Cards() {
       check(id);
     }
   }
+
+  console.log("tututCards", items);
 
   return (
     <div className="container">
